4b2fa9ee4038a5690f4bdd0d98047a2fa410b59a
Horological <azb40420@ucmo.edu>
2018-09-26 09:19:15 -0400

    Adding logging to project

--- Mining/Variables.py
+++ Mining/Variables.py
@@ -1,6 +1,11 @@
+import logging
+logging.info("Loading Variables")
 WORK = 0
+
 PEER_NODES = []
 MINER_NODE_URL = "127.0.0.1"
 PORT = 5000
 BLOCKCHAIN = []
 PENDING_TRANSACTIONS = []
+logging.debug("work: {} peers:{} node_url:{} port: {} blockchain: {} pending: {}".format(WORK,PEER_NODES,MINER_NODE_URL,PORT,BLOCKCHAIN,PENDING_TRANSACTIONS))
+logging.info("Done loading Variables")
