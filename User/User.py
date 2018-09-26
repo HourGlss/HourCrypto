b78da01fcab4a731a9cf5807f5be639fd9a7d29f
Horological <azb40420@ucmo.edu>
2018-09-26 09:19:31 -0400

    Adding logging to project

--- User/User.py
+++ User/User.py
@@ -1,4 +1,3 @@
-import hashlib
 password = "Desktop boys"
 private_key = "49ddb1037ed00bb40ee31ece4053fc4adf41ba7870fd522c104d40b524b0a4d3"
 public_key = "uI1QVm5l1Z5rwhPj5ebj2fNWxgbmgNwavzXiviW5mXs3Uw8ZQBnXE/uGOdUvqnNrnnHoKAouq87nWILB3iQTig=="
