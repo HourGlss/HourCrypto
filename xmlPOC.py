1078119fc6c5afd063ffc0a5c629aaf5bdc74e78
Xyf <1668760+HourGlss@users.noreply.github.com>
2018-11-02 00:56:40 -0400

    XML POC.... wow this is clumsy

--- xmlPOC.py
+++ xmlPOC.py
@@ -1,47 +1,61 @@
-import defusedxml.ElementTree as et
-tree = et.parse('scratch.xml')
-root = tree.getroot()
-blocks = []
+import xmltodict
+import requests
+
+# xml = """<?xml version='1.0' encoding='utf-8'?>
+# <a>б</a>"""
+# headers = {'Content-Type': 'application/xml'} # set what your server accepts
+# print(requests.post('http://httpbin.org/post', data=xml, headers=headers).text)
+# tree = et.parse('scratch.xml')
+
 
 class Block(object):
-    def __init__(self, id=0, foo=0, bar=0, trans=0):
+    def __init__(self, id=0, foo=0, bar=0, trans=[]):
         self.id = int(id)
         self.foo = float(foo)
         self.bar = str(bar)
         self.transactions = trans
 
-    def importxml(self, block_xml):
-        self.id = block_xml.find('id').text
-        self.bar = block_xml.find('bar').text
-        self.foo = block_xml.find('foo').text
-        import_transactions = block_xml.find('transactions')
-        transactions = []
-        for transaction in import_transactions:
-            t_from = transaction.find('from').text
-            t_to = transaction.find('to').text
-            t_amount = int(transaction.find("amount").text)
-            transactions.append({'from': t_from, 'to': t_to, 'amount': t_amount})
-        self.transactions = transactions
+    def importXml(self, block_xml):
+        for field in block_xml:
+            if field != "transactions":
+                block_to_add.setField(field, block_xml[field])
+            else:
+                for transaction in block_xml[field]['trans']:
+                    transaction_to_add = {}
+                    for k in transaction:
+                        transaction_to_add[k] = transaction[k]
+                    block_to_add.setField("transaction", transaction_to_add)
 
-    def exportxml(self,tabs):
-        xml = "\t" * tabs +"<block>\n"
-        tabs += 1
-        xml += "\t" * tabs +"<id>{}</id>\n".format(self.id)
-        xml += "\t" * tabs +"<foo>{}</foo>\n".format(self.foo)
-        xml += "\t" * tabs +"<bar>{}</bar>\n".format(self.bar)
-        xml += "\t" * tabs +"<transactions>\n"
-        tabs += 1
-        for trans in self.transactions:
-            xml += "\t" * tabs + "<trans>\n"
-            xml += "\t" * (tabs+1) + "<from>{}</from>\n".format(trans['from'])
-            xml += "\t" * (tabs+1) + "<to>{}</to>\n".format(trans['to'])
-            xml += "\t" * (tabs+1) + "<amount>{}</amount>\n".format(trans['amount'])
-            xml += "\t" * tabs+ "</trans>\n"
-        tabs -= 1
-        xml += "\t" * tabs +"</transactions>\n"
-        tabs -= 1
-        xml += "\t" * tabs +"<block>\n"
-        return xml
+
+    def setField(self,field,value):
+        if field == 'id':
+            try:
+                self.id = int(value)
+            except:
+                pass
+        elif field == 'foo':
+            try:
+                self.foo = float(value)
+            except:
+                pass
+        elif field == 'bar':
+            try:
+                self.bar = int(value)
+            except:
+                pass
+        elif field == 'transaction':
+            try:
+                if type(value) is type({}):
+
+                    self.transactions.append(value)
+
+            except:
+                pass
+
+
+    def exportXml(self):
+        block = {'block':{'id':self.id,'foo':self.foo,'bar':self.bar,'transactions':{'trans':self.transactions}}}
+        return xmltodict.unparse(block)
 
     def __repr__(self):
         return str(self)
@@ -49,15 +63,16 @@ class Block(object):
     def __str__(self):
         return "id:{},foo:{},bar:{},trans:{}".format(self.id, self.foo, self.bar, str(self.transactions))
 
-for block_xml in root.findall('block'):
-    b = Block()
-    b.importxml(block_xml)
-    blocks.append(b)
-xml = '<?xml version="1.0"?>\n'
-xml += '<data>\n'
-for block in blocks:
 
-    xml += block.exportxml(1)
-xml += '</data>'
-print(xml)
+xmldata = open('scratch.xml',"rb")
+tree2 = xmltodict.parse(xmldata)
+# root = tree.getroot()
+blocks = []
+for block_xml in tree2['root']['block']:
+    block_to_add = Block()
+    block_to_add.importXml(block_xml)
+    blocks.append(block_to_add)
+for block in blocks:
+    print(block.exportXml())
+
 
