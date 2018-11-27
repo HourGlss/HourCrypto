3fe783103ab4c46bbd653a52c89a0c2d0cbcfea4
Xyf <1668760+HourGlss@users.noreply.github.com>
2018-11-26 19:50:11 -0500

    Time consuming, but might fix networking.

--- Mining_classes/Mining.py
+++ Mining_classes/Mining.py
@@ -10,21 +10,31 @@ import inspect
 import logging
 
 
-def proof_of_work(last_block, data):
+def proof_of_work(data):
     func = inspect.currentframe().f_back.f_code
-    index_to_use = last_block.index + 1
+    last_block = None
     done = False
     now = None
     pow_hash_object = None
     effort = None
     while not done:
+        last_block = get_last_block()
         now = time.time()
         effort, pow_hash_object = Utility.genhash(index_to_use, now, data, last_block.hash)
         leading_zeroes = Utility.leadingzeroes(pow_hash_object.digest())
+        index_to_use = last_block.index + 1
         if leading_zeroes >= Variables.WORK:
             done = True
     return_block = Block(index_to_use, now, pow_hash_object.hexdigest(), effort, data, last_block.hash)
     return return_block
+
+def get_last_block():
+    url = "http://" + Variables.MINER_NODE_URL + ":" + str(Variables.PORT) + "/lastblock"
+    last_block_xml = requests.post(url)
+    parsed = xmltodict.parse(last_block_xml.content)
+    last_block = Block()
+    last_block.import_from_xml(parsed['block'])
+    return last_block
 
 
 def mine():
@@ -32,14 +42,10 @@ def mine():
     # See if other blockchains exist
     # TODO add consensus back
     while True:
-        url = "http://" + Variables.MINER_NODE_URL + ":" + str(Variables.PORT) + "/lastblock"
-        last_block_xml = requests.post(url)
-        parsed = xmltodict.parse(last_block_xml.content)
-        last_block = Block()
-        last_block.import_from_xml(parsed['block'])
+
         transactions = [{"from": "network", "to": User.public_key, "amount": 1}]
         # TODO get REAL transactions from transaction db
-        pow_output = proof_of_work(last_block, transactions)
+        pow_output = proof_of_work(transactions)
         transactions = [{}]
         url = "http://" + Variables.MINER_NODE_URL + ":" + str(Variables.PORT) + "/block"
         xml = pow_output.export_to_xml()
