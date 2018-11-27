ea3d689eca216a0073956927406d5b1e7d82ec83
Xyf <1668760+HourGlss@users.noreply.github.com>
2018-11-27 04:11:56 -0500

    Past me was an idiot.

--- Utilities/Utility.py
+++ Utilities/Utility.py
@@ -2,7 +2,7 @@ import base64
 import hashlib
 import secrets
 import string
-import Mining_classes.Variables as variables
+import Mining_classes.Variables as Variables
 import inspect
 import time
 from Blockchain_classes.Block import Block
@@ -13,7 +13,7 @@ def create_genesis_block():
     func = inspect.currentframe().f_back.f_code
     # logging.info("Creating a genesis block")
     # logging.debug("Work:{}".format(variables.WORK))
-    work_ez = int(variables.WORK / 4) + 1
+    work_ez = int(Variables.WORK / 4) + 1
     proof_of_work = "0" * work_ez
     pad = "1337"
     for i in range(4, 64):
@@ -24,12 +24,6 @@ def create_genesis_block():
     # logging.info("Returning block: {}".format(b))
     return b
 
-def buildmessage(origin, data):
-    func = inspect.currentframe().f_back.f_code
-    logging.debug("type: {} data:{}".format(origin, data))
-    return (origin, data)
-
-
 def buildpow(index,timestamp,effort,data,previous_hash):
     m = hashlib.sha256()
     m.update((str(index) + str(timestamp) + str(effort) + str(data) + str(previous_hash)).encode('utf-8'))
@@ -39,11 +33,15 @@ def validate(block):
 
     func = inspect.currentframe().f_back.f_code
 
+    if not leadingzeroes(block.proof_of_work) >= Variables.WORK:
+        logging.debug("Proof of work failed")
+        return False
+
     logging.info("Validating block")
     if block.index == 0:
         logging.debug("Block validated good")
         return True
-    generaged_proof_of_work = buildpow(block.index,block.timemade,block.effort,block.transactions,block.previous_hash)
+    generated_hash = buildpow(block.index,block.timemade,block.effort,block.transactions,block.previous_hash)
     '''
     self.index = int(listinfo[0])
         self.timemade = str(listinfo[1])
@@ -53,10 +51,10 @@ def validate(block):
         self.previous_hash = str(listinfo[6])
         self.hash = self.hash_block()
     '''
-    if block.proof_of_work == generaged_proof_of_work.hexdigest():
+    if block.hash == generated_hash.hexdigest():
         logging.debug("Block validated good")
         return True
-    logging.warning("powhexdig:{} should have received: {}".format(generaged_proof_of_work.hexdigest(),block.proof_of_work))
+    logging.warning("powhexdig:{} should have received: {}".format(generated_hash.hexdigest(),block.hash))
     return False
 
 
@@ -72,7 +70,6 @@ def genhash(index,timestamp,data,last_hash):
     return effort, buildpow(index,timestamp,effort,data,last_hash)
 
 def leadingzeroes(digest):
-    #TODO I'm almost positive there's a faster way to do this
     n = 0
     result = ''.join(format(x, '08b') for x in bytearray(digest))
     for c in result:
@@ -87,38 +84,6 @@ def createHexdigest(s):
     m.update(s.encode('utf-8'))
     secret_key = m.hexdigest()
     return secret_key
-
-def validate_blockchain(blockchain):
-    func = inspect.currentframe().f_back.f_code
-
-    logging.info("Validating blockchain")
-    previous = ""
-    for i in range(0,len(blockchain)-1):
-        block = blockchain[i]
-        logging.debug("Evaluating block: {}".format(block))
-        if block.index == 0:
-            logging.debug("Genesis block found")
-            previous = block.hash
-            continue
-        else:
-            previous = blockchain[i-1].hash
-        if not validate(block):
-            logging.warning("Did not validate blockchain block did not validate")
-
-            return False
-        data = block.data
-        for transaction in data:
-            logging.debug("trans: {}".format(transaction))
-            if transaction['from'] == "network" and transaction['amount'] != 1:
-                logging.warning("Did not validate blockchain mining wrong")
-
-                return False
-        if previous != block.previous_hash:
-            logging.debug("previous hash didn't validate previous:{} block.previous:{}".format(previous,block.previous_hash))
-            logging.warning("Did not validate blockchain Previous hash incorrect")
-            return False
-    logging.info("Validated")
-    return True
 
 def validate_signature(public_key, signature, message):
     #TODO I have never tested this
